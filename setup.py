f616fc2357c844303be910d4e54d0cf2ae56a443
parthls23 <2023ca49f@sigce.edu.in>
2026-05-14 11:19:05 +0530

    Complete Project

--- setup.py
+++ setup.py
@@ -9,18 +9,18 @@ def get_requirements(file_path:str)->List[str]:
     requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
-        requirements=[req.replace("\n","")for req in requirements]
+        requirements=[req.replace("\n","") for req in requirements]
 
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
-
-    return requirements        
+    
+    return requirements
 
 setup(
 name='mlproject',
 version='0.0.1',
-author='Parth',
-author_email='2023ca49f@sigce.edu.in',
+author='Krish',
+author_email='krishnaik06@gmail.com',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
 
